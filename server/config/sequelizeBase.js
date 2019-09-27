8cbb91d7ba2113d5f931005b08dc0c7cefbec768
shiyi <shiyi@zugeliang01.com>
2019-09-27 16:35:52 +0800

    no message

--- server/config/sequelizeBase.js
+++ server/config/sequelizeBase.js
@@ -3,7 +3,12 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize('shop_tmp','root','123456',{
 	host:'localhost',
     dialect:'mysql',
-    port: 3306
+    port: 3306,
+    pool: {
+        validateConnection: res => {
+            console.log('连接成功拉------------>', res)
+        }
+    }
 })
 
 module.exports = sequelize;
